3694eeb71d6aeb7db846cb8556d3e7d260942eb8
jeissonr115 <jeissonfeliper@gmail.com>
2024-11-15 22:21:26 -0500

    nose

--- src/components/JourneySpecial/JourneySpecial.jsx
+++ src/components/JourneySpecial/JourneySpecial.jsx
@@ -1,6 +1,10 @@
-// export const JourneySpecial = () => (
-//   <div className="journey-special">
-//     <BtnNewJourney />
-//     <TblNewJourney />
-//   </div>
-// );
+import { Layout } from "../../Layout";
+export const JourneySpecial = () => (
+  <Layout>
+    {
+      // crea un boton para crear un nuevo joruney
+      // al darle click al boton deve lanzar un poopup en donde este un formulario en donde tenga los sigientes datos
+      //fecha de solicitud, tipode usuario(empleado, externo), usuario requiere servicio, usuario aprueba, origen, destino, fecha servicio, hora servicio, centro de costo;
+    }
+  </Layout>
+);
